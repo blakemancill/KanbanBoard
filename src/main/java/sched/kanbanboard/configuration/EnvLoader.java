c329e24026d93588c8efb365f325a216e45b5258
Blake-Evelyn Mancill <kmanci6@wgu.edu>
2025-10-06 13:06:53 -0400

    Swapped out private constructor with lombok equivalent

--- src/main/java/sched/kanbanboard/configuration/EnvLoader.java
+++ src/main/java/sched/kanbanboard/configuration/EnvLoader.java
@@ -1,7 +1,10 @@
 package sched.kanbanboard.configuration;
 
 import io.github.cdimascio.dotenv.Dotenv;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class EnvLoader {
     public static void loadDotenv() {
         Dotenv dotenv = Dotenv.configure()
@@ -10,6 +13,4 @@ public class EnvLoader {
                 .load();
         dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
     }
-
-    private EnvLoader() {}
 }
