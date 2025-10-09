dafd172b66b7176ce49f71a9af51e975959a4915
Blake-Evelyn Mancill <kmanci6@wgu.edu>
2025-10-09 19:03:38 -0400

    Added exception handler for unexpected errors

--- src/main/java/sched/kanbanboard/exceptions/GlobalExceptionHandler.java
+++ src/main/java/sched/kanbanboard/exceptions/GlobalExceptionHandler.java
@@ -1,12 +1,20 @@
 package sched.kanbanboard.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex){
+        log.error("Unhandled exception occurred", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error");
+    }
 
     @ExceptionHandler(TaskBoardAlreadyExistsException.class)
     public ResponseEntity<String> handleTaskBoardAlreadyExistsException(TaskBoardAlreadyExistsException ex) {
